3d0b5fcae9657456516361a5c3d5825bfa013d70
dikshyantg <46819356+dikshyantg@users.noreply.github.com>
2023-01-12 22:52:09 -0600

    Update changing_names.py

--- changing_names.py
+++ changing_names.py
@@ -1,51 +1,36 @@
+#import libraries
 import os
 import pandas as pd 
 import shutil
+
+#set file path and read csv file
 path = "/Users/dikshyantgautam/downloads/nachusa_2019_2nd/"
 file_name = "/Users/dikshyantgautam/Desktop/site_extract.csv"
 file_input = pd.read_csv(file_name, ",")
+
+#create new directory
 path_2 = path + "renamed/"
 os.makedirs(path_2,exist_ok= True) # argument 
-# making dictionary , write meaningful names
+
+#create dictionary from csv file
 site = {}
 for sitename, year in zip(file_input["Site Name"],file_input["Year of Planting"]):
     site[sitename]=year
     
-
-#print (file_input)
-
+#iterate over files in directory
 for nachusa in os.listdir(path,):
     if not nachusa.endswith(".fastq"):
         continue
-    
+    #print file name
     print (nachusa)
     date= nachusa.split("_")[3] 
     m = date[0:2]
     d = date[2:4]
-    y = date[4:6] # variable name small  ,class name small
+    y = date[4:6] 
     siteextr = nachusa.split("_")[2]
-    
-    #print(siteextr)
     extr_date = site.get(siteextr,"")
-    
-    # if siteextr in site: 
-    #     extr_date = site[siteextr]
-    # else: 
-    #     extr_date = ""
-
     out = f"{extr_date}_{siteextr}_20{y}-{m}-{d}.fastq"
     dest = path_2 + out
     src = path + nachusa 
     if not os.path.exists(dest):
         shutil.copy(src,dest)
-    
-
-
-
-# variable syntax , string syntax 
-# python object definition 
-# function, variable, string , object property access ( class and its access), object vs class 
-
-
-
-    
